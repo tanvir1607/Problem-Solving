e9ebf034f930a86f74bd61fad66c311a1dc4a0d9
tanvir1607 <10viranik@gmail.com>
2026-08-01 12:47:50 +0600

    J. Primes from 1 to n

---
+++ C/J_Primes_from_1_to_n.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+// bool isPrime(int num)
+// {
+//     if (num == 1) return false;
+//     for (int i = 2; i < num; i++)
+//         if (num % i == 0) return false;
+//     return true;
+// }
+
+
+bool isPrime(int num)
+{
+    if (num == 1) return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0) return false;
+    return true;
+}
+
+
+int main() 
+{
+    int num;
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+        if (isPrime(i)) 
+            printf("%d ", i);
+
+    return 0;
+}
