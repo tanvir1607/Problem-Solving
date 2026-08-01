b24e462dd2eed8972729c74cca502ee57be1461a
tanvir1607 <10viranik@gmail.com>
2026-08-01 11:52:40 +0600

    A. 1 to N

--- C/A_1_to_N.c
+++ C/A_1_to_N.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) printf("%d\n", i);
+    for (int i = 1; i <= n; i++)
+        printf("%d\n", i);
 
     return 0;
 }
