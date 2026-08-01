7f371d64434ff311f55dcf757e2da75e23994996
tanvir1607 <10viranik@gmail.com>
2026-08-01 10:51:40 +0600

    H. One Prime

--- Python/H_One_Prime.py
+++ Python/H_One_Prime.py
@@ -2,11 +2,20 @@ def main():
     num = int(input())
     print("YES" if isPrime(num) else "NO")
 
+
+# def isPrime(num):
+#     if num == 1: return False
+#     for i in range(2, num):
+#         if num % i == 0: return False
+#     return True
+
+
 def isPrime(num):
     if num == 1: return False
-    for i in range(2, num):
+    for i in range(2, int(num ** 0.5 + 1)):
         if num % i == 0: return False
     return True
+
 
 if __name__ == "__main__":
     main()
